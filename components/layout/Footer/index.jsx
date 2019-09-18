32a2e59a7a69ebfe692f34c168f02b53fe6d03a0
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2019-09-18 21:15:00 +0530

    feat: footer design done.

--- components/layout/Footer/index.jsx
+++ components/layout/Footer/index.jsx
@@ -1,4 +1,6 @@
 import React, { Fragment, Component } from "react";
+import Link from 'next/link'
+import Container from '@material-ui/core/Container';
 
 import "./style.scss";
 
@@ -6,7 +8,47 @@ export default class Footer extends Component {
   render() {
     return (
       <Fragment>
-        <div className="footer">Copyright &copy; 2019</div>
+        <div className="footer">
+          <Container fixed>
+            <ul className="links">
+              <li>
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/Terms">
+                  <a>Terms</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/privacy-policy">
+                  <a>Privacy policy</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/cookies">
+                  <a>Cookies</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/jobs">
+                  <a>Jobs</a>
+                </Link>
+
+              </li>
+              <li>
+                <Link href="/settings">
+                  <a>Settings</a>
+                </Link>
+              </li>
+            </ul>
+
+            <div className="copyright">
+              © 2019 Neta .Ltd
+            </div>
+          </Container>
+        </div>
       </Fragment>
     );
   }
