d746b1c29d7fe117f5438cb9c2f88d63d6eb1285
Pankaj Jasoria <pankaj@toaster.co>
2019-10-15 17:15:52 +0530

    ref: minor file changes.

--- functions/utils/admin.js
+++ functions/utils/admin.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-var serviceAccount = require("../../serviceAccountKey.json");
+var serviceAccount = require("../serviceAccountKey.json");
 
 
 admin.initializeApp({
